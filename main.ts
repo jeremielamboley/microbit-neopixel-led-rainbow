f738bd428c416e0475d1cdb4c8a1e098fc5eee02
Jeremie Lamboley <43125035+jeremielamboley@users.noreply.github.com>
2021-12-23 23:27:28 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+basic.forever(function () {
+    strip.rotate(1)
+    strip.show()
+    basic.pause(200)
+})
