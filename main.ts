fc98f6056031dfdbf3d1c9206018d95f058d63bb
Jeremie Lamboley <43125035+jeremielamboley@users.noreply.github.com>
2024-03-23 12:00:20 +0900

    new button control

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    LEDdirection = forward
+})
+input.onButtonPressed(Button.B, function () {
+    LEDdirection = backward
+})
+let LEDdirection = 0
+let backward = 0
+let forward = 0
+basic.showIcon(IconNames.Heart)
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+forward = 1
+backward = -1
+LEDdirection = forward
 strip.showRainbow(1, 360)
 basic.forever(function () {
-    strip.rotate(1)
+    strip.rotate(LEDdirection)
     strip.show()
     basic.pause(200)
 })
