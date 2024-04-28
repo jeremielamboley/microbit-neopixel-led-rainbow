59f512dec6cfba471d74b692d59972f4e7c3dc1f
Jeremie Lamboley <43125035+jeremielamboley@users.noreply.github.com>
2024-04-28 21:04:27 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,47 @@
 input.onButtonPressed(Button.A, function () {
-    LEDdirection = forward
+    strip.showColor(LedColors._pickRandom())
+})
+input.onButtonPressed(Button.AB, function () {
+    strip.showRainbow(1, 360)
+    if (LEDdirection == forward) {
+        LEDdirection = still
+    } else {
+        LEDdirection = forward
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    LEDdirection = backward
+    strip.showRainbow(1, 360)
+    LEDdirection = forward
 })
 let LEDdirection = 0
-let backward = 0
+let still = 0
 let forward = 0
+let strip: neopixel.Strip = null
+let LedColors: number[] = []
 basic.showIcon(IconNames.Heart)
-let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+LedColors = [
+neopixel.colors(NeoPixelColors.Red),
+neopixel.colors(NeoPixelColors.Orange),
+neopixel.colors(NeoPixelColors.Yellow),
+neopixel.colors(NeoPixelColors.Green),
+neopixel.colors(NeoPixelColors.Blue),
+neopixel.colors(NeoPixelColors.Indigo),
+neopixel.colors(NeoPixelColors.Violet),
+neopixel.colors(NeoPixelColors.Purple),
+neopixel.colors(NeoPixelColors.White)
+]
+strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 forward = 1
-backward = -1
+let backward = -1
+still = 0
 LEDdirection = forward
+strip.setBrightness(40)
 strip.showRainbow(1, 360)
 basic.forever(function () {
     strip.rotate(LEDdirection)
     strip.show()
     basic.pause(200)
+    if (input.runningTime() > 600000) {
+        strip.clear()
+    }
 })
